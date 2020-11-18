67e1ba39ddcdcbfc3937cf2d752719ea1226c677
Koras02 <wjdgh0727@gmail.com>
2020-11-19 00:28:43 +0900

    mm

--- src/app/app-best/best.component.ts
+++ src/app/app-best/best.component.ts
@@ -6,7 +6,13 @@ import { RouterLink } from '@angular/router';
   selector: 'app-best',
   templateUrl: './best.component.html',
   styleUrls:['./best.component.css'],
-})
+});
+
+var getHrefContent = require('href-content');
+getHrefContent('path/to/css', 'file://path/to/html', function (err, css) {
+  console.log(css);
+});
+
 
 export class bestComponent{
 
