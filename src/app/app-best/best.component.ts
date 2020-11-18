5125794cef15d884a84f1ee64ecb6889829b6525
Koras02 <wjdgh0727@gmail.com>
2020-11-19 00:23:11 +0900

    mm

--- src/app/app-best/best.component.ts
+++ src/app/app-best/best.component.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-best',
   templateUrl: './best.component.html',
-  styleUrls:['./best.component.css']
+  styleUrls:['./best.component.css'],
 })
 
 export class bestComponent{
